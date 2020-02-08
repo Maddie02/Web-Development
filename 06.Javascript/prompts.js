d887cd5b4483bea32e1653bad69018e98ad13c67
Madlen <madlensarkisian@gmail.com>
2020-02-08 17:15:31 +0200

    Add basic html

---
+++ 06.Javascript/prompts.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What's your first name?")
+var lastName = prompt("What's your last name?")
+var age = prompt("How old are you?")
+
+alert(firstName + lastName)
+alert("Your age is " + age + " years old")
